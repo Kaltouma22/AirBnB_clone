dc71c056170ea8d5ce7316b89f95232ed8a35097
kaltouma22 <kchbiki363@gmail.com>
2024-02-06 15:34:35 +0100

    done

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" contains the entry point of the command interpreter """
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """ Exit the program """
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program by typing EOF (Ctrl+D on Linux, Ctrl+Z on windows)"""
+        return True
+
+    def emptyline(self):
+        """Do nothing on empty line"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
