4e824b8c4ee85b6f956ecbbe166b76f39b8024fa
MZM010 <mohamedzouine827@gmail.com>
2024-02-07 22:54:11 +0100

    8

--- console.py
+++ console.py
@@ -43,6 +43,48 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** no instance found **")
 
+    def do_destroy(self, args):
+        my_args = args.split()
+        if len(my_args) == 0:
+            print("** class name missing **")
+            return
+        elif my_args[0] != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        elif len(my_args) == 1:
+            print("** instance id missing **")
+            return
+        for key in storage.all():
+            if storage.all()[key].id == my_args[1]:
+                del storage.all()[key]
+                storage.save()
+                return
+        print("** no instance found **")
+                
+    def do_update(self, args):
+        my_args = args.split()
+        if len(my_args) == 0:
+            print("** class name missing **")
+            return
+        elif my_args[0] != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        elif len(my_args) == 1:
+            print("** instance id missing **")
+            return
+        else:
+            instance_id = my_args[1]
+            attribute_name = str(my_args[2])
+            attribute_value = str(my_args[3])
+
+            for key in storage.all():
+                if key.split('.')[1] == instance_id:
+                    instance = storage.all()[key]
+                    setattr(instance, attribute_name, attribute_value)
+                    instance.save()
+                    return
+
+            print("** no instance found **")
 
     def do_all(self, args):
         my_args = args.split()
